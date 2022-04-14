426d75de5b9675b30ed650e7dca3478c340cf0f4
AZor1973 <azor@mail.ru>
2022-04-14 10:40:20 +0300

    Hard Code

---
+++ src/main/java/ru/azor/springmail/lesson_8/MailService.java
@@ -0,0 +1,29 @@
+package ru.azor.springmail.lesson_8;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Service
+@RequiredArgsConstructor
+public class MailService {
+
+    private final JavaMailSender javaMailSender;
+//    Антипатерн Hard Code
+    @PostConstruct
+    public void sendMail() {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom("AZorMailSender");
+        simpleMailMessage.setTo("azor@mail.ru");
+        simpleMailMessage.setSubject("Turning on the laptop");
+        simpleMailMessage.setText("Hello, from AZor! Your notebook is started!\n"
+                + LocalDateTime.now().format(DateTimeFormatter
+                .ofPattern("dd.MM.yyyy HH:mm:ss")));
+        javaMailSender.send(simpleMailMessage);
+    }
+}
